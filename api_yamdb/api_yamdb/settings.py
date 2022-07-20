9d2ea35a5406c8743b8ca82d6e7d537a1fa1f8d2
Ахметжанов Ильдар <ma9or@mail.ru>
2022-07-20 23:50:45 +0600

    add test

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -65,8 +65,6 @@ WSGI_APPLICATION = "api_yamdb.wsgi.application"
 
 DATABASES = {
     'default': {
-        #'ENGINE': os.getenv('DB_ENGINE', default='django.db.backends.postgresql'),
-        #'NAME': os.getenv('DB_NAME'),
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         'USER': os.getenv('POSTGRES_USER'),
